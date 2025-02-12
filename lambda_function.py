5efbb1744b92bcc6b059bb501c792e25e1dec90d
plebnyan <nlt1166@outlook.com>
2025-02-12 15:34:19 +0700

    update lambda file

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,33 @@
 import json
+import boto3
+import pandas as pd
+from io import StringIO
+from datetime import date
+from get_data import get_jobs, get_last_page  
 
 def lambda_handler(event, context):
-    # TODO implement
+    today = date.today().strftime('%Y%m%d')
+    file_name = f"scraped_data/jobs_{today}.csv"
+
+    bucket_name = "jobnet-pipeline"
+    s3_client = boto3.client("s3")
+    csv_buffer = StringIO()
+
+    # Fetch job listings
+    last_page = get_last_page()
+    jobs = get_jobs(last_page)
+
+    # Convert DataFrame to CSV
+    jobs.to_csv(csv_buffer, index=False)
+
+    # Upload to S3
+    s3_client.put_object(
+        Bucket=bucket_name,
+        Key=file_name,
+        Body=csv_buffer.getvalue()
+    )
+
     return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        "statusCode": 200,
+        "body": json.dumps(f"Successfully uploaded {file_name} to {bucket_name}")
     }
